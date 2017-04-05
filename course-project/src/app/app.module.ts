a242caecfc6784b34578fd00b0f82bdcdb9860f8
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-04-05 23:13:40 +0200

    create all components

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecepieListComponent } from './recipes/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './recipes/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './recipes/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecepieListComponent,
+    RecepieDetailComponent,
+    RecepieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
