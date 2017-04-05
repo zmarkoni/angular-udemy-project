746f3e54545efa1806a6f4f62947775f3676bdcd
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-04-06 00:16:50 +0200

    create recipe list HTML and use recipe model

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecepieListComponent } from './recipes/recepie-list/recepie-list.component';
-import { RecepieDetailComponent } from './recipes/recepie-detail/recepie-detail.component';
-import { RecepieItemComponent } from './recipes/recepie-list/recepie-item/recepie-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -17,9 +17,9 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecepieListComponent,
-    RecepieDetailComponent,
-    RecepieItemComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
---
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('Recepie Name', 'Some description', 'http://lorempixel.com/400/200/food/')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
