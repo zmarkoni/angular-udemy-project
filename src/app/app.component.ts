79ebabded173527b915ea574f04aa01df055f6b0
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-04-09 22:59:14 +0200

    passing Recipe data with propery binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'app works!';
-  loadedFeature:string = 'recipe';
+  loadedFeature:string = 'recipes';
 
   onNavigate(feature:string) {
     this.loadedFeature = feature;
