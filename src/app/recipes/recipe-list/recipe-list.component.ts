618a7beb4043deceb0f87aadbbba303afa8823e4
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-04-11 00:24:26 +0200

    passing data to recipe detail component when click on recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from "../recipe.model";
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-      new Recipe('Recepie Name', 'Some description', 'http://lorempixel.com/400/200/food/'),
-      new Recipe('Recepie Name 1', 'Some description 1', 'http://lorempixel.com/400/200/food/'),
-      new Recipe('Recepie Name 2', 'Some description 2', 'http://lorempixel.com/400/200/food/')
-  ];
+    recipes: Recipe[] = [
+        new Recipe('Recepie Name', 'Some description', 'http://lorempixel.com/400/200/food/'),
+        new Recipe('Recepie Name 1', 'Some description 1', 'http://lorempixel.com/400/200/food/'),
+        new Recipe('Recepie Name 2', 'Some description 2', 'http://lorempixel.com/400/200/food/')
+    ];
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
 
 }
